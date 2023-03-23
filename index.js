861cba6d7c544df51bc60cacde91ea29a586040a
“Simona <simona.ignacz@gmail.com>
2023-03-23 13:17:52 +0100

    hotovo

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log('funguju!');
+
+let hodeneCislo = 1
+const kocka = document.querySelector('.dice')
+const hodKockou = () => {
+    hodeneCislo = hodeneCislo + 1
+    if (hodeneCislo === 7) {
+        hodeneCislo = 1
+    }
+    kocka.src = `img/side${hodeneCislo}.svg`
+}
+document.addEventListener('keydown', hodKockou)
